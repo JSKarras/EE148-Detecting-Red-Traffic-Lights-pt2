6f48d9bdd39b642c75a35c33f9840465fd998266
Johanna Karras <johannakarras@gmail.com>
2021-04-12 22:49:04 -0700

    Finished convolution and prediction functionality

--- generate_split.py
+++ generate_split.py
@@ -1,28 +1,34 @@
+''' Split data into training and testing datasets. '''
 import numpy as np
 import os
 
 np.random.seed(2020) # to ensure you always get the same train/test split
 
-data_path = '../data/RedLights2011_Medium'
-gts_path = '../data/hw02_annotations'
-split_path = '../data/hw02_splits'
+data_path = '/Users/Johanna/Desktop/Computer Vision/hw01/RedLights2011_Medium'
+gts_path = '../data/hw02_annotations/'
+split_path = '../data/hw02_splits/'
+preds_path = '../data/hw02_predictions/'
 os.makedirs(preds_path, exist_ok=True) # create directory if needed
 
-split_test = False # set to True and run when annotations are available
-
+split_test = True # False splits filenames only, True splits images into separate files
 train_frac = 0.85
 
 # get sorted list of files:
 file_names = sorted(os.listdir(data_path))
-
 # remove any non-JPEG files:
 file_names = [f for f in file_names if '.jpg' in f]
-
 # split file names into train and test
 file_names_train = []
 file_names_test = []
+
 '''
-Your code below. 
+My code below. 
+'''
+file_names_train = np.random.choice(file_names, int(len(file_names)*0.85), replace=False)
+file_names_test = [f for f in file_names if f not in file_names_train]
+
+'''
+My code above. 
 '''
 
 assert (len(file_names_train) + len(file_names_test)) == len(file_names)
@@ -39,13 +45,22 @@ if split_test:
     # annotations
     gts_train = {}
     gts_test = {}
+    
     '''
-    Your code below. 
+    My code below. 
+    '''
+    for filename in file_names_train:
+        gts_train[filename] = gts[filename]
+    for filename in file_names_test:
+        gts_test[filename] = gts[filename]
+
+    '''
+    My code above. 
     '''
     
     with open(os.path.join(gts_path, 'annotations_train.json'),'w') as f:
         json.dump(gts_train,f)
-    
+
     with open(os.path.join(gts_path, 'annotations_test.json'),'w') as f:
         json.dump(gts_test,f)
     
--- run_predictions.py
+++ run_predictions.py
@@ -10,19 +10,28 @@ def compute_convolution(I, T, stride=None):
     convolution at each location. You can add optional parameters (e.g. stride, 
     window_size, padding) to create additional functionality. 
     '''
-    (n_rows,n_cols,n_channels) = np.shape(I)
-
+    (n_rows, n_cols, n_channels) = np.shape(I)
+    
     '''
     BEGIN YOUR CODE
     '''
-    heatmap = np.random.random((n_rows, n_cols))
+    # Get shape of template
+    (t_rows, t_cols, t_channels) = np.shape(T)
+    
+    # Add padding to preserve shape of image
+    x_pad, y_pad = t_rows-1, t_cols-1
+    I = np.pad(I, ((x_pad, x_pad), (y_pad, y_pad), (0,0)), 'constant') # ((top, bot),(left,right))
+    
+    heatmap = np.zeros((n_rows, n_cols))
+    for row in range(0, n_rows, stride):
+        for col in range(0, n_cols, stride):
+            conv = np.sum(T * I[row:row+t_rows,col:col+t_cols]) / ((t_rows+1)*(t_cols+1)) # * 255
+            heatmap[row, col] = conv
 
     '''
     END YOUR CODE
     '''
-
     return heatmap
-
 
 def predict_boxes(heatmap):
     '''
@@ -35,35 +44,46 @@ def predict_boxes(heatmap):
     '''
     BEGIN YOUR CODE
     '''
+    (n_rows, n_cols, n_channels) = np.shape(I)
+    (h_rows, h_cols) = np.shape(heatmap)
     
-    '''
-    As an example, here's code that generates between 1 and 5 random boxes
-    of fixed size and returns the results in the proper format.
-    '''
+    # Compute size of boxes based on image and heatmap
+    box_height = box_size
+    box_width = box_size
 
-    box_height = 8
-    box_width = 6
+    for row in range(0, h_rows, stride):
+        for col in range(0, h_cols, stride):
+            ul_x, ul_y = row, col
+            br_x, br_y = row + box_height, col + box_width
+            score = heatmap[row, col]
+            # Enforce threshold minimum score of convolution
+            if score  > threshold:
+            	# Check that bounding box is not too close to another bounding box
+                flag = True
+                for [x1, y1, x2, y2, s] in output:
+                    dist1 = np.sqrt((x1-ul_x)**2 + (y1-ul_y)**2)
+                    dist2 = np.sqrt((x2-ul_x)**2 + (y2-ul_y)**2)
+                    if dist1 < 20 or dist2 < 20:
+                        flag = False
 
-    num_boxes = np.random.randint(1,5)
-
-    for i in range(num_boxes):
-        (n_rows,n_cols,n_channels) = np.shape(I)
-
-        tl_row = np.random.randint(n_rows - box_height)
-        tl_col = np.random.randint(n_cols - box_width)
-        br_row = tl_row + box_height
-        br_col = tl_col + box_width
-
-        score = np.random.random()
-
-        output.append([tl_row,tl_col,br_row,br_col, score])
+                if flag:
+                    output.append([ul_x, ul_y, br_x, br_y, score])
 
     '''
     END YOUR CODE
     '''
 
     return output
-
+    
+# Helper function to normalize images w.r.t to kernel
+def normalize_image(img, kernel):
+    img_normalized = img.copy().astype(np.float)
+    r, g, b = np.mean(kernel[:, :, 0]), np.mean(kernel[:, :, 1]), np.mean(kernel[:, :, 2])
+    img_normalized[:, :, 0] = 3 * np.maximum(np.subtract(img_normalized[:, :, 0], r), 0) / 255
+    img_normalized[:, :, 1] = 0.1*np.maximum(np.subtract(img_normalized[:, :, 1], g), 0) / 255
+    img_normalized[:, :, 2] = 0.1*np.maximum(np.subtract(img_normalized[:, :, 2], b), 0) / 255
+    
+    return img_normalized 
 
 def detect_red_light_mf(I):
     '''
@@ -84,14 +104,25 @@ def detect_red_light_mf(I):
     '''
     BEGIN YOUR CODE
     '''
-    template_height = 8
-    template_width = 6
-
     # You may use multiple stages and combine the results
-    T = np.random.random((template_height, template_width))
-
-    heatmap = compute_convolution(I, T)
-    output = predict_boxes(heatmap)
+    T = Image.open('kernel.png').convert(mode='RGB') # base kernel
+    I_normalized = normalize_image(I, np.asarray(T))
+    I = I_normalized
+    
+    output = []
+    thresholds = [0.264, 0.435, 0.55, 0.6, 0.6, 0.6]
+    T_sizes = [3, 5, 10, 20, 30, 50]
+    for i in range(len(T_sizes)):
+        T_size, threshold = T_sizes[i], thresholds[i]
+        T_resized = T.copy().resize((T_size, T_size))
+        T_resized = np.asarray(T_resized)
+        T_normalized = normalize_image(T_resized, T_resized)
+        
+        T_resized = T_normalized
+    
+        heatmap = compute_convolution(I, T_resized, stride)
+        predicted_boxes = predict_boxes(I, heatmap, threshold, stride, T_size)
+        output.extend(predicted_boxes)
 
     '''
     END YOUR CODE
@@ -99,6 +130,9 @@ def detect_red_light_mf(I):
 
     for i in range(len(output)):
         assert len(output[i]) == 5
+        if (output[i][4] < 0.0) or (output[i][4] > 1.0):
+            print(output[i][4])
+        
         assert (output[i][4] >= 0.0) and (output[i][4] <= 1.0)
 
     return output
