0c2a1cc6b4577c9a966013cdc32ab536c4edbd6d
Johanna Karras <johannakarras@gmail.com>
2021-04-14 22:29:39 -0700

    Completed eval_detector.py functionality

--- eval_detector.py
+++ eval_detector.py
@@ -1,19 +1,53 @@
-import os
-import json
-import numpy as np
-
+'''Evaluate model performance. '''
 def compute_iou(box_1, box_2):
     '''
     This function takes a pair of bounding boxes and returns intersection-over-
     union (IoU) of two bounding boxes.
     '''
-    iou = np.random.random()
+    [x1, y1, x2, y2] = box_1
     
-    assert (iou >= 0) and (iou <= 1.0)
+    [x3, y3, x4, y4] = box_2
 
+    # Compute area of intersection
+    area_intersection = max(0, min(x2, x4) - max(x1, x3) + 1) * max(0, min(y2, y4) - max(y1, y3) + 1)
+
+    # Compute area of union = area_box1 + area_box2 - area_intersection
+    area_union = (abs(x1-x2) * abs(y1-y2)) + (abs(x3-x4) * abs(y3-y4)) - area_intersection
+    
+    # IoU = area of intersection / area of union
+    iou = area_intersection / area_union
+
+    assert (iou >= 0) and (iou <= 1.0)
     return iou
 
+# Helper function: visualize ground truth and predicted bounding boxes 
+def visualize_gt_and_preds(I, preds, gts,  filename):
+    fig, ax = plt.subplots()
+    plt.imshow(I)
+    preds = preds[filename] # predicted bounding boxes
+    for [ul_x, ul_y, br_x, br_y, score] in preds:
+        (x, y) = (ul_x, ul_y)
+        rect = matplotlib.patches.Rectangle((y, x), -abs(ul_x - br_x), -abs(br_y - ul_y),
+                                 linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
 
+    gt = gts[filename] # predicted ground truth labels
+    for [ul_x, ul_y, br_x, br_y] in gt:
+        (x, y) = (ul_x, ul_y)
+        rect = matplotlib.patches.Rectangle((y, x), abs(ul_x - br_x), abs(br_y - ul_y),
+                                 linewidth=1, edgecolor='g', facecolor='none')
+        
+        max_iou = 0.0
+        for pred in preds:
+            iou = compute_iou(pred, [ul_x, ul_y, br_x, br_y])
+            if iou > max_iou:
+                max_iou = iou
+        
+        plt.text(y, x, str(max_iou), color="white")
+        ax.add_patch(rect)
+    
+    plt.show()
+    
 def compute_counts(preds, gts, iou_thr=0.5, conf_thr=0.5):
     '''
     This function takes a pair of dictionaries (with our JSON format; see ex.) 
@@ -25,25 +59,68 @@ def compute_counts(preds, gts, iou_thr=0.5, conf_thr=0.5):
     <gts> is a dictionary containing ground truth bounding boxes for a
     collection of images.
     '''
-    TP = 0
-    FP = 0
-    FN = 0
+    TP, FP, FN = 0, 0, 0
 
     '''
     BEGIN YOUR CODE
     '''
-    for pred_file, pred in preds.iteritems():
+    # Total ground truth objects
+    N =  np.sum([len(gts[file]) for file, pred in preds.items()])
+    # Total predictions
+    M = np.sum([len(pred) for file, pred in preds.items()]) 
+    for pred_file, pred in preds.items():
         gt = gts[pred_file]
-        for i in range(len(gt)):
-            for j in range(len(pred)):
+        found = False
+        for i in range(len(gt)): # Iterate ground truth objects
+            for j in range(len(pred)): # Iterate predictions
                 iou = compute_iou(pred[j][:4], gt[i])
-
-
+                confidence = pred[j][4]
+                #print(confidence)
+                # apply threshold and confidence to determine True Positives (TP)
+                if not found and iou > iou_thr and confidence >= conf_thr: 
+                    TP += 1
+                    found = True
+    # False Positives (FP) = Predictions - True Positives (TP)
+    FP = M - TP
+    # False Negatives (FN) = Ground Truth Objects - True Positives (TP)
+    FN = N - TP
+    
+    assert (TP + FP == M)
+    assert (TP + FN == N)
+    
     '''
     END YOUR CODE
     '''
+    return int(TP), int(FP), int(FN)
 
-    return TP, FP, FN
+''' Get precision and recall values for a given IOU Threshold. '''
+def get_pr(iou_thr):
+    confidence_thrs = np.linspace(0.0, 0.44, 5)
+    tp_train = np.zeros(len(confidence_thrs))
+    fp_train = np.zeros(len(confidence_thrs))
+    fn_train = np.zeros(len(confidence_thrs))
+    precision = []
+    recall = []
+    for i, conf_thr in enumerate(confidence_thrs):
+        tp_train[i], fp_train[i], fn_train[i] = compute_counts(preds_train, gts_train, iou_thr=iou_thr, conf_thr=conf_thr)
+
+        # Visualize
+        '''
+        filename = file_names_train[i]
+        I = Image.open(os.path.join(data_path,filename))
+        I = np.asarray(I)
+        visualize_gt_and_preds(I, preds_train, gts_train, filename)
+        '''
+        # Print results
+        #print("Confidence Threshold = ", conf_thr)
+        print("TP = ", tp_train[i], ", FP = ", fp_train[i], ", FN = ", fn_train[i])
+
+        precision.append(tp_train[i]/(tp_train[i] + fp_train[i]))
+        recall.append(tp_train[i]/(tp_train[i] + fn_train[i]))
+    return precision, recall
+    
+# path for image data
+data_path = '/Users/Johanna/Desktop/Computer Vision/hw01/RedLights2011_Medium'
 
 # set a path for predictions and annotations:
 preds_path = '../data/hw02_preds'
@@ -52,7 +129,7 @@ gts_path = '../data/hw02_annotations'
 # load splits:
 split_path = '../data/hw02_splits'
 file_names_train = np.load(os.path.join(split_path,'file_names_train.npy'))
-file_names_test = np.load(os.path.join(split_Path,'file_names_test.npy'))
+file_names_test = np.load(os.path.join(split_path,'file_names_test.npy'))
 
 # Set this parameter to True when you're done with algorithm development:
 done_tweaking = False
@@ -81,16 +158,17 @@ if done_tweaking:
 
 # For a fixed IoU threshold, vary the confidence thresholds.
 # The code below gives an example on the training set for one IoU threshold. 
-
-
-confidence_thrs = np.sort(np.array([preds_train[fname][4] for fname in preds_train],dtype=float)) # using (ascending) list of confidence scores as thresholds
-tp_train = np.zeros(len(confidence_thrs))
-fp_train = np.zeros(len(confidence_thrs))
-fn_train = np.zeros(len(confidence_thrs))
-for i, conf_thr in enumerate(confidence_thrs):
-    tp_train[i], fp_train[i], fn_train[i] = compute_counts(preds_train, gts_train, iou_thr=0.5, conf_thr=conf_thr)
-
+    
 # Plot training set PR curves
+for iou_thr in [0.1, 0.25, 0.3, 0.35, 0.5, 0.75]:
+    p, r = get_pr(iou_thr)
+    plt.plot(r, p)
+    plt.scatter(r, p)
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim(0, 0.3)
+    plt.title('PR Curve for IOU Threshold of '+ str(iou_thr))
+    plt.show()
 
 if done_tweaking:
     print('Code for plotting test set PR curves.')
--- run_predictions.py
+++ run_predictions.py
@@ -3,7 +3,8 @@ import numpy as np
 import json
 from PIL import Image
 
-def compute_convolution(I, T, stride=None):
+''' Helper functions for running predictions. '''
+def compute_convolution(I, T, stride=1, window_size=None, padding=None):
     '''
     This function takes an image <I> and a template <T> (both numpy arrays) 
     and returns a heatmap where each grid represents the output produced by 
@@ -33,7 +34,7 @@ def compute_convolution(I, T, stride=None):
     '''
     return heatmap
 
-def predict_boxes(heatmap):
+def predict_boxes(I, heatmap, threshold, stride=1, box_size=10):
     '''
     This function takes heatmap and returns the bounding boxes and associated
     confidence scores.
@@ -51,41 +52,54 @@ def predict_boxes(heatmap):
     box_height = box_size
     box_width = box_size
 
+    print("Box size = ", box_size)
+    num_boxes = 0
     for row in range(0, h_rows, stride):
         for col in range(0, h_cols, stride):
             ul_x, ul_y = row, col
             br_x, br_y = row + box_height, col + box_width
             score = heatmap[row, col]
-            # Enforce threshold minimum score of convolution
             if score  > threshold:
-            	# Check that bounding box is not too close to another bounding box
                 flag = True
+                # Check that bounding boxes do not overlap
                 for [x1, y1, x2, y2, s] in output:
                     dist1 = np.sqrt((x1-ul_x)**2 + (y1-ul_y)**2)
                     dist2 = np.sqrt((x2-ul_x)**2 + (y2-ul_y)**2)
                     if dist1 < 20 or dist2 < 20:
                         flag = False
-
+                mean_flux = np.mean(I[ul_x:br_x, ul_y:br_y])
+                if box_size > 3 and (mean_flux > 1.1*0.105):
+                    flag = False
+                if box_size == 3 and (mean_flux > 1.05*0.105 or mean_flux < 0.9*0.105):
+                    flag = False
+                
                 if flag:
+                    print("Avg Pixel Flux = ", np.mean(I[ul_x:br_x, ul_y:br_y]))
                     output.append([ul_x, ul_y, br_x, br_y, score])
+                    num_boxes += 1
 
+    #print("Num Boxes Predicted for threshold ", threshold, " = ", num_boxes)
     '''
     END YOUR CODE
     '''
 
     return output
-    
-# Helper function to normalize images w.r.t to kernel
+
 def normalize_image(img, kernel):
+    '''
+    For an image patch or kernel, img, normalize it with respect to each color channel using
+    the mean of each color channel of the full image, I. Return the normalized img.
+    '''
     img_normalized = img.copy().astype(np.float)
     r, g, b = np.mean(kernel[:, :, 0]), np.mean(kernel[:, :, 1]), np.mean(kernel[:, :, 2])
+
     img_normalized[:, :, 0] = 3 * np.maximum(np.subtract(img_normalized[:, :, 0], r), 0) / 255
     img_normalized[:, :, 1] = 0.1*np.maximum(np.subtract(img_normalized[:, :, 1], g), 0) / 255
     img_normalized[:, :, 2] = 0.1*np.maximum(np.subtract(img_normalized[:, :, 2], b), 0) / 255
-    
+        
     return img_normalized 
 
-def detect_red_light_mf(I):
+def detect_red_light_mf(I, stride=1):
     '''
     This function takes a numpy array <I> and returns a list <output>.
     The length of <output> is the number of bounding boxes predicted for <I>. 
@@ -110,8 +124,8 @@ def detect_red_light_mf(I):
     I = I_normalized
     
     output = []
-    thresholds = [0.264, 0.435, 0.55, 0.6, 0.6, 0.6]
-    T_sizes = [3, 5, 10, 20, 30, 50]
+    thresholds = [0.264, 0.435, 0.62, 0.63] # 0.264,
+    T_sizes = [3, 5]
     for i in range(len(T_sizes)):
         T_size, threshold = T_sizes[i], thresholds[i]
         T_resized = T.copy().resize((T_size, T_size))
@@ -119,7 +133,7 @@ def detect_red_light_mf(I):
         T_normalized = normalize_image(T_resized, T_resized)
         
         T_resized = T_normalized
-    
+        
         heatmap = compute_convolution(I, T_resized, stride)
         predicted_boxes = predict_boxes(I, heatmap, threshold, stride, T_size)
         output.extend(predicted_boxes)
@@ -137,14 +151,37 @@ def detect_red_light_mf(I):
 
     return output
 
+# Visualize predictions
+def visualize_bounding_boxes(I, bounding_boxes):
+    '''
+    This function takes a numpy image arry <I> and a list of
+    <bounding_boxes> and displays I with bounding boxes. 
+    Each element of <bounding boxes> is a 4-integer list 
+    specifying the top left and bottom right corners of bounding 
+    boxes contained in the image I. 
+    '''
+    fig, ax = plt.subplots()
+    plt.imshow(I)
+    idx = 0
+    for [ul_x, ul_y, br_x, br_y, score] in bounding_boxes:
+        (x, y) = (ul_x, ul_y)
+        rect = matplotlib.patches.Rectangle((y, x), -abs(ul_x - br_x), -abs(br_y - ul_y),
+                                 linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+        plt.text(y, x, str(round(score, 4)), color='white')
+        idx += 1
+        
+    plt.show()
+    
+''' Run predictions! '''
 # Note that you are not allowed to use test data for training.
 # set the path to the downloaded data:
-data_path = '../data/RedLights2011_Medium'
+data_path = '/Users/Johanna/Desktop/Computer Vision/hw01/RedLights2011_Medium'
 
 # load splits: 
 split_path = '../data/hw02_splits'
 file_names_train = np.load(os.path.join(split_path,'file_names_train.npy'))
-file_names_test = np.load(os.path.join(split_Path,'file_names_test.npy'))
+file_names_test = np.load(os.path.join(split_path,'file_names_test.npy'))
 
 # set a path for saving predictions:
 preds_path = '../data/hw02_preds'
@@ -158,15 +195,17 @@ Make predictions on the training set.
 '''
 preds_train = {}
 for i in range(len(file_names_train)):
+    print("Image: ", i+1)
+    # read image
+    img = Image.open(os.path.join(data_path,file_names_train[i]))
+    #img = Image.open('small_image.png').convert('RGB')
+    img = np.asarray(img)
 
-    # read image using PIL:
-    I = Image.open(os.path.join(data_path,file_names_train[i]))
-
-    # convert to numpy array:
-    I = np.asarray(I)
-
-    preds_train[file_names_train[i]] = detect_red_light_mf(I)
-
+    # detect red lights in image
+    preds_train[file_names_train[i]] = detect_red_light_mf(img, stride=1)
+    # show bounding boxes
+    visualize_bounding_boxes(img, preds_train[file_names_train[i]])
+    
 # save preds (overwrites any previous predictions!)
 with open(os.path.join(preds_path,'preds_train.json'),'w') as f:
     json.dump(preds_train,f)
@@ -180,10 +219,10 @@ if done_tweaking:
 
         # read image using PIL:
         I = Image.open(os.path.join(data_path,file_names_test[i]))
-
+        
         # convert to numpy array:
         I = np.asarray(I)
-
+        
         preds_test[file_names_test[i]] = detect_red_light_mf(I)
 
     # save preds (overwrites any previous predictions!)
